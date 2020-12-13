67d22e7966610ebe30972e9b637f49dfbdb18f32
maokun.liu <maokun.liu@qunar.com>
2020-12-13 16:27:21 +0800

    修复一个在python2下可能出现的问题

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="puffidea",
-    version="0.0.3",
+    version="0.0.2",
     author="puff",
     author_email="angrypuff333@gmail.com",
     description="获取Jetbrains激活码",
-    long_description=long_description,
+    long_description=open("README.md", 'r').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/PeterPuffer/puffidea",
     packages=setuptools.find_packages(),
